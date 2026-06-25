0b0ef215ad551fdee642995cc94085b9e88a2244
twdhpltools-dotcom <twdhpltools@gmail.com>
2026-06-25 19:20:31 +0530

    Add files via upload

--- think-doc-portal-v2-0/assets/js/main.js
+++ think-doc-portal-v2-0/assets/js/main.js
@@ -34,6 +34,40 @@ function getOTP() {
 }
 /*-------------------------------------- OTP-INPUT ----------------------------------------*/
 
+/*-------------------------------------- CUSTOM-MODAL ----------------------------------------*/
+function openBookingSetupTLModal() {
+  const modal = document.getElementById("BookingSetupTLModal");
+  const box = document.getElementById("BookingSetupTLModalBox");
+
+  modal.classList.remove("hidden");
+  modal.classList.add("flex");
+
+  setTimeout(() => {
+    box.classList.remove("opacity-0", "scale-95");
+    box.classList.add("opacity-100", "scale-100");
+  }, 10);
+}
+
+function closeCustomModal() {
+  const modal = document.getElementById("BookingSetupTLModal");
+  const box = document.getElementById("BookingSetupTLModalBox");
+
+  box.classList.add("opacity-0", "scale-95");
+  box.classList.remove("opacity-100", "scale-100");
+
+  setTimeout(() => {
+    modal.classList.add("hidden");
+    modal.classList.remove("flex");
+  }, 300);
+}
+
+document.getElementById("BookingSetupTLModal").addEventListener("click", function(e) {
+  if (e.target.id === "BookingSetupTLModal") {
+    closeCustomModal();
+  }
+});
+/*-------------------------------------- CUSTOM-MODAL ----------------------------------------*/
+
 /*-------------------------------------- CUSTOM-SELECT ----------------------------------------*/
 // const doctorBtn = document.getElementById("DoctorProfileSelect");
 // const doctorDropdown = document.getElementById("DoctorDropdown");
@@ -59,6 +93,42 @@ function getOTP() {
 //     }
 // });
 /*-------------------------------------- CUSTOM-SELECT ----------------------------------------*/
+
+/*-------------------------------------- MENU-TOGGLE ----------------------------------------*/
+function openMenuToggle() {
+  const sidebar = document.getElementById("sidebar");
+
+  sidebar.classList.toggle("translate-y-full");
+  sidebar.classList.toggle("opacity-0");
+
+  sidebar.classList.toggle("translate-y-0");
+  sidebar.classList.toggle("opacity-100");
+}
+/*-------------------------------------- MENU-TOGGLE ----------------------------------------*/
+
+/*-------------------------------------- SHOW/HIDE-FUCNTIONALITY ----------------------------------------*/
+function toggleSubMenu(btn) {
+  const subMenu = btn.nextElementSibling;
+
+  subMenu.classList.toggle("max-h-0");
+  subMenu.classList.toggle("opacity-0");
+
+  subMenu.classList.toggle("max-h-[180px]");
+  subMenu.classList.toggle("opacity-100");
+}
+
+function bookingDetailsShow() {
+  const optionBox = document.getElementById("BookingDetailsOption");
+
+  optionBox.classList.toggle("py-0");
+  optionBox.classList.toggle("max-h-0");
+  optionBox.classList.toggle("opacity-0");
+
+  optionBox.classList.toggle("py-4");
+  optionBox.classList.toggle("max-h-[80px]");
+  optionBox.classList.toggle("opacity-100");
+}
+/*-------------------------------------- SHOW/HIDE-FUCNTIONALITY ----------------------------------------*/
 
 /*-------------------------------------- PRE-LOADER ----------------------------------------*/
 window.addEventListener("load", function () {
