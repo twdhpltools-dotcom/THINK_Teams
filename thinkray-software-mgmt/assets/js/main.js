b847971d40f70ae60fd2dd0ae4b94f4f92e415f3
twdhpltools-dotcom <twdhpltools@gmail.com>
2026-09-15 17:11:54 +0530

    Add files via upload

--- thinkray-software-mgmt/assets/js/main.js
+++ thinkray-software-mgmt/assets/js/main.js
@@ -215,19 +215,4 @@ function setTextColor(color) {
   document.getElementById("liveEditor").focus();
 }
 /*-------------------------------------- HTML-LIVE-EDIT ----------------------------------------*/
-
-/*-------------------------------------- TAB-CONTENT ----------------------------------------*/
-function openTab(btn) {
-  document.querySelectorAll(".tab-content").forEach(content=>{
-    content.classList.add("hidden");
-  });
-
-  document.querySelectorAll(".tab-btn").forEach(button=>{
-    button.classList.remove("active");
-  });
-
-  document.getElementById(btn.dataset.tab).classList.remove("hidden");
-  btn.classList.add("active");
-}
-/*-------------------------------------- TAB-CONTENT ----------------------------------------*/
 /*------------------------------------------- JS FOR MAIN COMPLETE ------------------------------------------*/
