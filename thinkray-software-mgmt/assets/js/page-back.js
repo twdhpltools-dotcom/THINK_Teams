e3a85d8a0e8d8f9cf9c5fcbff59e94743231fb69
twdhpltools-dotcom <twdhpltools@gmail.com>
2026-09-22 10:50:34 +0530

    Delete thinkray-software-mgmt/assets/js directory

--- thinkray-software-mgmt/assets/js/page-back.js
+++
@@ -1,8 +0,0 @@
-/*------------------------------------------- JS FOR PG-BACK-FUCNTIONALITY START ------------------------------------------*/
-document.querySelectorAll(".go-back-btn").forEach((btn) => {
-  btn.addEventListener("click", function (e) {
-    e.preventDefault();
-    window.history.back();
-  });
-});
-/*-------------------------------------- JS FOR PG-BACK-FUCNTIONALITY COMPLETE ----------------------------------------*/
--- thinkray-software-mgmt/assets/js/pre-loader.js
+++
@@ -1,5 +0,0 @@
-/*------------------------------------------- JS FOR PRE-LOADER START ------------------------------------------*/
-window.addEventListener("load", function () {
-  document.getElementById("PreLoader").classList.add("hide");
-});
-/*-------------------------------------- JS FOR PRE-LOADER COMPLETE ----------------------------------------*/
